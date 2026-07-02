7c7630364738800a324949a77b7aa77abe62a180
OxiCuza <oxicusa@gmail.com>
2026-07-02 08:34:12 +0700

    feat: pending state

--- src/components/LikeToggle.tsx
+++ src/components/LikeToggle.tsx
@@ -1,5 +1,6 @@
-import { Heart } from "lucide-react";
+import { Heart, LoaderCircle } from "lucide-react";
 import { useLiked } from "../context/liked-context";
+import { useState } from "react";
 
 export function LikeButton({
     id
@@ -7,23 +8,36 @@ export function LikeButton({
     id: number
 }) {
     const {liked, setLiked} = useLiked();
+    const [pending, setPending] = useState(false);
+
     return (
         <button 
             className="group"
             onClick={() => {
-                if (liked.includes(id)) {
-                    setLiked(liked.filter((likedId) => likedId !== id))
-                } else {
-                    setLiked([...liked, id])
-                }
+                setPending(true);
+                
+                setTimeout(() => {
+                    if (liked.includes(id)) {
+                        setLiked(liked.filter((likedId) => likedId !== id))
+                    } else {
+                        setLiked([...liked, id])
+                    }
+                    setPending(false);
+                }, 1000);
+
             }}>
-            <Heart 
-                className={
-                    liked.includes(id)
-                        ? "fill-pink-500 stroke-none"
-                        : "stroke-slate-200 group-hover:stroke-slate-300"
-                } 
-            />
+            
+            {pending ? (
+                <LoaderCircle className="animate-spin stroke-slate-300" />
+            ) : (
+                <Heart 
+                    className={
+                        liked.includes(id)
+                            ? "fill-pink-500 stroke-none"
+                            : "stroke-slate-200 group-hover:stroke-slate-300"
+                    } 
+                />
+            )}
         </button>
     )
 }
--- src/components/PuppyForm.tsx
+++ src/components/PuppyForm.tsx
@@ -1,5 +1,6 @@
 import { Dispatch, SetStateAction, useState } from "react";
 import { Puppy } from "../types";
+import { useFormStatus } from "react-dom";
 
 export function PuppyForm({
     puppies,
@@ -11,7 +12,10 @@ export function PuppyForm({
     return (
         <div className="mt-12 flex items-center justify-between bg-white p-8 shadow ring ring-black/5">
             <form 
-                action={(formData: FormData) => {
+                action={async (formData: FormData) => {
+
+                    await new Promise((resolve) => setTimeout(resolve, 2000));
+                    
                     const newPuppy: Puppy = {
                         id: puppies.length + 1,
                         name: formData.get("name") as string,
@@ -26,6 +30,7 @@ export function PuppyForm({
                     <fieldset className="flex w-full flex-col gap-1">
                     <label htmlFor="name">Name</label>
                     <input
+                        required
                         className="max-w-96 rounded-sm bg-white px-2 py-1 ring ring-black/20 focus:ring-2 focus:ring-cyan-500 focus:outline-none"
                         id="name"
                         type="text"
@@ -35,6 +40,7 @@ export function PuppyForm({
                     <fieldset className="flex w-full flex-col gap-1">
                     <label htmlFor="trait">Personality trait</label>
                     <input
+                        required
                         className="max-w-96 rounded-sm bg-white px-2 py-1 ring ring-black/20 focus:ring-2 focus:ring-cyan-500 focus:outline-none"
                         id="trait"
                         type="text"
@@ -54,13 +60,22 @@ export function PuppyForm({
                     />
                     </fieldset>
                 </div>
-                <button
-                    className="mt-4 inline-block rounded bg-cyan-300 px-4 py-2 font-medium text-cyan-900 hover:bg-cyan-200 focus:ring-2 focus:ring-cyan-500 focus:outline-none"
-                    type="submit"
-                >
-                    Add puppy
-                </button>
+                <SubmitButton />
             </form>
         </div>
+    )
+}
+
+function SubmitButton() {
+    const status = useFormStatus();
+    
+    return (
+        <button
+            className="mt-4 inline-block rounded bg-cyan-300 px-4 py-2 font-medium text-cyan-900 hover:bg-cyan-200 focus:ring-2 focus:ring-cyan-500 focus:outline-none disabled:bg-slate-200 disabled:cursor-not-allowed"
+            disabled={status.pending}
+            type="submit"
+        >
+            {status.pending ? `Adding ${status?.data?.get("name")}...` : "Add puppy"}
+        </button>
     )
 }
